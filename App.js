d78179880a18814f40d699e42aec8871a2ac76f3
Narendra Sisodiya <narendra@billexo.com>
2018-08-18 10:08:05 +0800

    small change.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-
+import Expo from "expo";
 export default class App extends React.Component {
   render() {
     return (
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'green',
+    paddingTop: Expo.Constants.statusBarHeight,
     alignItems: 'center'
   },
   mainText: {
